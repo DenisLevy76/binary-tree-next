e3ae164a211e844955c47afac4d547a365327585
MichSide <michel-amoral@hotmail.com>
2021-04-28 15:06:45 -0300

    LogoBar Finished

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../components/Header';
+// import NavigationBar from '../components/NavigationBar'
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Header />
+      {/* <NavigationBar/> */}
       <Component {...pageProps} />
     </div>
   );
