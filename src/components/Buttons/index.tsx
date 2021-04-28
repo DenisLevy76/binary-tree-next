58f25dd9b60d5b99ecaffcad7424582dedea691e
Denis Levy <denis.levi11@gmail.com>
2021-04-28 19:05:56 -0300

    Finished the buttons component

--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -4,7 +4,7 @@ import { ContextTree } from '../../contexts/ContextTree';
 import styles from './styles.module.css';
 
 const Buttons: React.FC = () => {
-  const {createNewTree} = useContext(ContextTree)
+  const {createNewTree, handleButtonPreOrder, handleButtonInOrder, handleButtonPostOrder, handleButtonBFS} = useContext(ContextTree)
   const [nNodes, setNNodes] = useState(0);
 
   return (
@@ -12,10 +12,10 @@ const Buttons: React.FC = () => {
       <h2 className={styles.h2}>Algoritmos</h2>
       <input type="number" className={styles.inputNumber} onChange={(e) => setNNodes(Number(e.target.value))} placeholder="Número de nós"/>
       <button type="button" className={styles.button} onClick={() => createNewTree(nNodes)}>Gerar árvore</button>
-      <button type="button" className={styles.button} >Pré-ordem</button>
-      <button type="button" className={styles.button} >Em ordem</button>
-      <button type="button" className={styles.button} >Pós-ordem</button>
-      <button type="button" className={styles.button} >Em largura</button>
+      <button type="button" className={styles.button} onClick={handleButtonPreOrder}>Pré-ordem</button>
+      <button type="button" className={styles.button} onClick={handleButtonInOrder}>Em ordem</button>
+      <button type="button" className={styles.button} onClick={handleButtonPostOrder}>Pós-ordem</button>
+      <button type="button" className={styles.button} onClick={handleButtonBFS}>Em largura</button>
     </div>
   );
 }
