c3d1fcc14b72fe2671c518022f3dab1f820b6360
MichSide <michel-amoral@hotmail.com>
2021-04-29 09:37:06 -0300

    Update Fork 3

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -3,13 +3,20 @@ import styles from './styles.module.css'
 export default function NagivationBar() {
     return (
         <div className={styles.NavContainer}>
-            <img className={styles.imgHome}src="/home.png" alt="Home"/>
-            <p>Home</p>
-            <div className={styles.separate}>
-                <img className={styles.imgStaff}src="/staff.png" alt="Staff"/>
-                <a className={styles.Center}>Staff</a>
-            </div>
-            
+          <a href="/">
+              <div className={styles.rectangle}>
+                  <img className={styles.imgHome}src="/home.png" alt="Home"/>
+                  <a className={styles.home}>Home</a>
+              </div>
+          </a>
+          <a href="/staff">
+              <div className={styles.separate}>
+                  <div className={styles.rectangle}>
+                      <img className={styles.imgStaff}src="/staff.png" alt="Staff"/>
+                      <a>Staff</a>
+                  </div>
+              </div>
+          </a>  
         </div>
     )
 }
