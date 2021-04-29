02700f3600587864c5af989d7966c10212a66b5e
Denis Levy <denis.levi11@gmail.com>
2021-04-29 14:48:05 -0300

    fixed the overflow in the TreeGraph

--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -4,7 +4,7 @@ import { ContextTree } from '../../contexts/ContextTree';
 import styles from './styles.module.css';
 
 const Buttons: React.FC = () => {
-  const {createNewTree, handleButtonPreOrder, handleButtonInOrder, handleButtonPostOrder, handleButtonBFS} = useContext(ContextTree)
+  const {createNewTree, preOrder, postOrder, inOrder, handleButtonClick, handleButtonBFS} = useContext(ContextTree)
   const [nNodes, setNNodes] = useState(0);
 
   return (
@@ -12,9 +12,9 @@ const Buttons: React.FC = () => {
       <h2 className={styles.h2}>Algoritmos</h2>
       <input type="number" className={styles.inputNumber} onChange={(e) => setNNodes(Number(e.target.value))} placeholder="Número de nós"/>
       <button type="button" className={styles.button} onClick={() => createNewTree(nNodes)}>Gerar árvore</button>
-      <button type="button" className={styles.button} onClick={handleButtonPreOrder}>Pré-ordem</button>
-      <button type="button" className={styles.button} onClick={handleButtonInOrder}>Em ordem</button>
-      <button type="button" className={styles.button} onClick={handleButtonPostOrder}>Pós-ordem</button>
+      <button type="button" className={styles.button} onClick={() => handleButtonClick("Pré-ordem:", preOrder)}>Pré-ordem</button>
+      <button type="button" className={styles.button} onClick={() => handleButtonClick("Em ordem:", inOrder)}>Em ordem</button>
+      <button type="button" className={styles.button} onClick={() => handleButtonClick("Pós-ordem", postOrder)}>Pós-ordem</button>
       <button type="button" className={styles.button} onClick={handleButtonBFS}>Em largura</button>
     </div>
   );
