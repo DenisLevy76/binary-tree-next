6e8411e5163fb3f7b99ce54c241d79db87377437
Denis Levy <denis.levi11@gmail.com>
2021-05-30 18:42:07 -0300

    refactored methods preOrder, inOrder and postOrder && ajust ContextTree for this update

--- src/utils/tree.ts
+++ src/utils/tree.ts
@@ -1,119 +1,135 @@
 interface RawNodeDatum {
-  name: string;
-  attributes?: Record<string, string>;
-  children?: RawNodeDatum[];
+  name: string
+  attributes?: Record<string, string>
+  children?: RawNodeDatum[]
 }
 
-export class NodeTree{
-  constructor(value: number){
-    this.value = value;
+export class NodeTree {
+  constructor(value: number) {
+    this.value = value
   }
 
-  value: number;
-  left: NodeTree = null;
-  right: NodeTree = null;
+  value: number
+  left: NodeTree = null
+  right: NodeTree = null
 
-  insert(value: number){
-    if (this.value){
-      if(value < this.value){
-        this.left? this.left.insert(value) : this.left = new NodeTree(value)
-      }else{
-        this.right? this.right.insert(value) : this.right = new NodeTree(value)
+  insert(value: number): void {
+    if (this.value) {
+      if (value < this.value) {
+        this.left ? this.left.insert(value) : (this.left = new NodeTree(value))
+      } else {
+        this.right
+          ? this.right.insert(value)
+          : (this.right = new NodeTree(value))
       }
     }
   }
 
-  preOrder(){
-    console.log(this.value);
-    this.left?.preOrder();
-    this.right?.preOrder();
+  preOrder(): number[] {
+    const list = []
+
+    list.push(this.value)
+    const left = this.left ? this.left.preOrder() : []
+    const right = this.right ? this.right.preOrder() : []
+
+    return [...list, ...left, ...right]
   }
 
-  inOrder(){
-    this.left?.inOrder();
-    console.log(this.value);
-    this.right?.inOrder();
+  inOrder(): number[] {
+    const list = []
+
+    const left = this.left ? this.left.inOrder() : []
+    list.push(this.value)
+    const right = this.right ? this.right.inOrder() : []
+
+    return [...left, ...list, ...right]
   }
 
-  postOrder(){
-    this.left?.postOrder();
-    this.right?.postOrder();
-    console.log(this.value);
+  postOrder(): number[]{
+    const list = []
+
+    const left = this.left ? this.left.postOrder() : []
+    const right = this.right ? this.right.postOrder() : []
+    list.push(this.value)
+
+    return [...left, ...right, ...list]
   }
 
-  BFS(){ //breadth-first search
-    const visited : NodeTree[] = [this];
-    let i = 0;
-    while(i < visited.length){
-      visited[i].left && visited.push(visited[i].left);
-      visited[i].right && visited.push(visited[i].right);
-      i++;
+  BFS(): number[]{
+    //breadth-first search
+    const visited: NodeTree[] = [this]
+    let i = 0
+    while (i < visited.length) {
+      visited[i].left && visited.push(visited[i].left)
+      visited[i].right && visited.push(visited[i].right)
+      i++
     }
 
-    return visited;
+    return visited.map(e => e.value)
   }
 
-  convertToRawNodeDatum(){
-    const RawNodeDatumLeft: RawNodeDatum = this.left?.convertToRawNodeDatum();
-    const RawNodeDatumRight: RawNodeDatum = this.right?.convertToRawNodeDatum();
-    const children:RawNodeDatum[]  = [];
+  convertToRawNodeDatum() {
+    const RawNodeDatumLeft: RawNodeDatum = this.left?.convertToRawNodeDatum()
+    const RawNodeDatumRight: RawNodeDatum = this.right?.convertToRawNodeDatum()
+    const children: RawNodeDatum[] = []
 
-    if (RawNodeDatumLeft){
-      children.push(RawNodeDatumLeft);
-    }else{
-      children.push({name: ''})
+    if (RawNodeDatumLeft) {
+      children.push(RawNodeDatumLeft)
+    } else {
+      children.push({ name: "" })
     }
 
-    if (RawNodeDatumRight){
-      children.push(RawNodeDatumRight);
-    }else{
-      children.push({name: ''})
+    if (RawNodeDatumRight) {
+      children.push(RawNodeDatumRight)
+    } else {
+      children.push({ name: "" })
     }
 
-    if (children.length > 0){
+    if (children.length > 0) {
       return {
         name: String(this.value),
         children
-      } as RawNodeDatum;
-    }
-    else{
+      } as RawNodeDatum
+    } else {
       return {
-        name: String(this.value),
-      } as RawNodeDatum;
+        name: String(this.value)
+      } as RawNodeDatum
     }
   }
 
-  getHeight(){
-    const left = this.left?.getHeight() || 0;
-    const right = this.right?.getHeight() || 0;
+  getHeight() {
+    const left = this.left?.getHeight() || 0
+    const right = this.right?.getHeight() || 0
 
-    if (left > right) return left + 1;
-    else return right + 1;
+    if (left > right) return left + 1
+    else return right + 1
   }
 
-  getBalanceFactor(){
-    const leftHeight = this.left?.getHeight() || 0;
-    const rightHeight = this.right?.getHeight() || 0;
+  getBalanceFactor() {
+    const leftHeight = this.left?.getHeight() || 0
+    const rightHeight = this.right?.getHeight() || 0
 
-    return Math.abs(leftHeight - rightHeight);
+    return Math.abs(leftHeight - rightHeight)
   }
 
-  isBalanced(){
+  isBalanced() {
     //is leaf?
-    if(this.left === null && this.right === null) return true;
+    if (this.left === null && this.right === null) return true
 
     // if the right and left sides are balanced and the current node's balancing factor is less than 1, return true
     // (Gambiarra: when there is no side it 't doesnexecute isBalanced because of the "?" Sign, so it returns undefined. Since the
     // side does not exist there is no way it is unbalanced so I consider this case to be valid.)
 
-    return (this.left?.isBalanced() || this.left?.isBalanced() == undefined) &&
+    return (
+      (this.left?.isBalanced() || this.left?.isBalanced() == undefined) &&
       (this.right?.isBalanced() || this.right?.isBalanced() == undefined) &&
-      this.getBalanceFactor() <= 1;
+      this.getBalanceFactor() <= 1
+    )
   }
 
-  toString(){
-    const left = this.left ? `${this.left.toString()}` : '';
-    const right = this.right ? `${this.right.toString()}` : '';
+  toString() {
+    const left = this.left ? `${this.left.toString()}` : ""
+    const right = this.right ? `${this.right.toString()}` : ""
 
     return `(${this.value}${left}${right})`
   }
